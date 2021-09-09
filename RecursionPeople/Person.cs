eb1a77906755c36507d52038cac0b54b79065c9e
Angel Gomez <agomez@gteam.es>
2021-09-09 19:14:05 +0200

    Cambios en el idioma para que no le de una neura a Edu al leerlo (Hola Eduuu)

---
+++ RecursionPeople/Person.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RecursionPeople {
+    class Person {
+        string name;
+        public Person(string name) {
+            this.name = name;
+        }
+        public void Greet(Person p1, Person p2) {
+            Console.WriteLine($"{p1.name} said something to {p2.name}");
+        }
+    }
+}
--- RecursionPeople/Program.cs
+++ RecursionPeople/Program.cs
@@ -3,26 +3,25 @@
 namespace RecursionPeople {
     class Program {
         static void Main(string[] args) {
-            Persona Juan = new Persona("Juan");
-            Persona Pedro = new Persona("Pedro");
-            Persona Angel = new Persona("Angel");
-            Persona Selene = new Persona("Selene");
-            Persona David = new Persona("David");
-            Persona Ana = new Persona("Ana");
-            Persona Nacho = new Persona("Nacho");
+            Person Juan = new Person("Juan");
+            Person Pedro = new Person("Pedro");
+            Person Angel = new Person("Angel");
+            Person Selene = new Person("Selene");
+            Person David = new Person("David");
+            Person Ana = new Person("Ana");
+            Person Nacho = new Person("Nacho");
             //Nacho.Saludar(Nacho, Juan);
-            Persona[] personas = {Juan, Pedro, Angel, Selene, David, Ana, Nacho};
-            Console.WriteLine("Se ha dado una cantidad de saltos de: " + calcularSaltos(Juan, Nacho, 0, 0, personas));
-
+            Person[] personas = {Juan, Pedro, Angel, Selene, David, Ana, Nacho};
+            Console.WriteLine("Se ha dado una cantidad de saltos de: " + CalcularDistance(Juan, Nacho, 0, 0, personas));
         }
-        static int calcularSaltos(Persona persona1, Persona persona2, int firstPosition, int cantidadSaltos, Persona[] personas) {
-            if(persona2.Equals(personas[firstPosition + 1])) {
-                return cantidadSaltos+1;
+        static int CalcularDistance(Person persona1, Person persona2, int firstPosition, int quantityOfJumps, Person[] people) {
+            if(persona2.Equals(people[firstPosition + 1])) {
+                return quantityOfJumps+1;
             }
-            5personas[cantidadSaltos].Saludar(personas[cantidadSaltos], personas[cantidadSaltos + 1]);
-            cantidadSaltos+=1;
+            5people[quantityOfJumps].Greet(people[quantityOfJumps], people[quantityOfJumps + 1]);
+            quantityOfJumps+=1;
             firstPosition += 1;
-            return calcularSaltos(persona1,  persona2, firstPosition, cantidadSaltos, personas);
+            return CalcularDistance(persona1,  persona2, firstPosition, quantityOfJumps, people);
         }
 
     }
